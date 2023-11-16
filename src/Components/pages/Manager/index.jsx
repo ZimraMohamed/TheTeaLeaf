4a1b98d182fa9ffcd0a2cd413c5711011b650f0d
NishadiSandeepani <nishadisandeepani26@gmail.com>
2023-11-16 10:59:34 +0530

    manager Supabase

--- src/Components/pages/Manager/index.jsx
+++ src/Components/pages/Manager/index.jsx
@@ -6,150 +6,147 @@ import Swal from 'sweetalert2';
 import supabase from '../../../supa/supabase/supabaseClient';
 
 
-//added
-const Manager = () =>{
+const Manager = () => {
+  
   const [formData, setFormData] = useState({
-    full_name: '',
+    fullName: '',
     m_id: '',
     Personal_id: '',
-    DOB: '',
-    email: '',
-    contact: '',
+    dateOfBirth: '',
+    Email: '',
+    Contact: '',
     password: '',
   });
 
-    const showAlert = () => {
-        alert("Your are successfully Registerd.");
-      };
-    
-      const validateForm = () => {
-   
-        const fullName = document.getElementById('full_name').value;
-        const managerId = document.getElementById('m_id').value;
-        const Personal_id = document.getElementById('Personal_id').value;
-        const dateOfBirth = document.getElementById('DOB').value;
-        const Email = document.getElementById('email').value;
-        const Contact = document.getElementById('contact').value;
-        if (fullName.trim() === '') {
-          alert('Please enter your full name.');
-          return false;
-          }
-        
-          if (managerId.trim() === '') {
-          alert('Please enter your"manager id.');
-          return false;
-          }
-        
-          if (Personal_id.trim() === '') {
-          alert('Please enter your personal id.');
-          return false;
-          }
-          if(    dateOfBirth.trim() === '' ||
-          Email.trim() === '' ||
-          Contact.trim() === '')
-          {
-          alert('Please fill out all fields.');
-          return false;
-          }
-          Swal.fire({
-            title: 'Manager',
-            text: "You've successfully registered", 
-          });
-    //added
-          insertDataIntoSupabase();
+  const showAlert = (message) => {
+    alert(message);
+  };
 
-          return false;
-          
-    
-        };
+  const gatDataIntoSupabase = async (e) => {
+    e.preventDefault();
 
-        const insertDataIntoSupabase = async () => {
-          try {
-            const { data, error } = await supabase
-              .from('Manager') 
-              .insert([
-                {
-                  full_name: formData.full_name,
-                  m_id: formData.m_id,
-                  Personal_id: formData.Personal_id,
-                  DOB: formData.DOB,
-                  email: formData.email,
-                  contact: formData.contact,
-                  password: formData.password,
-                },
-              ]);
-      
-            if (error) {
-              console.error('Error inserting data into Supabase:', error.message);
-            } else {
-              console.log('Data inserted into Supabase:', data);
-              showAlert();
-            }
-          } catch (error) {
-            console.error('Error connecting to Supabase:', error.message);
-          }
-        };
+    const personalId = document.getElementById('Personal_id').value;
+    const fullName = document.getElementById('full_name').value;
+    const dateOfBirth = document.getElementById('DOB').value;
+    const email = document.getElementById('email').value;
+    const contact = document.getElementById('contact').value;
+    const password = document.getElementById('password').value;
 
-        const handleInputChange = (e) => {
-          setFormData({
-            ...formData,
-            [e.target.name]: e.target.value
-          });
-        };
-
-       
-        
-        return(
-        <div class="background-image4">
-        <div className="manager">
-        <div class="manager-container">
-        <form action="/submit-form" method="POST" onSubmit={(e)=>{   
-            e.preventDefault(); 
-              if (validateForm()) {
-                document.forms[0].submit();
-          }
-              }}>
-          <h1 id="header_18" class="form-header" data-component="header">Manager</h1>
-      <div class="form-group">
-        <label for="full_name">Full Name</label>
-        <input type="text" class="form-control" id="full_name" name="full_name" placeholder="Enter your full name"/>
-      </div>
-      <div class="form-group">
-        <label for="m_id">Manager ID</label>
-        <input type="text" class="form-control" id="m_id" name="m_id" placeholder="Enter your manager id"/>
-      </div>
-      <div class="form-group">
-        <label for="Personal_id">NIC</label>
-        <input type="text" class="form-control" id="Personal_id" name="Personal_id" placeholder="Enter your NIC number"/>
-      </div>
-      <div class="form-group">
-        <label for="email">Email</label>
-        <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email address"/>
-      </div>
-      <div class="form-group">
-        <label for="DOB">Date of Birth</label>
-        <input type="date" class="form-control" id="DOB" name="DOB"/>
-      </div>
-      <div class="form-group">
-        <label for="contact">Contact</label>
-        <input type="contact" class="form-control" id="contact" name="contact" placeholder="Enter your contact numbers"/>
-      </div>
-      <div class="form-group">
-        <label for="password">Password</label>
-        <input type="password" class="form-control" id="password" name="password" placeholder="Enter your password"  onChange={handleInputChange}/>
-      </div>
-      <br/>
-      <button type="submit" class="btn btn-primary" >Submit</button>
-    </form>
     
-    
-    </div>
-        </div>
-        </div>
-        
-      
-      )
+    if (!fullName || !personalId || !dateOfBirth || !email || !contact || !password) {
+      showAlert('Please fill out all fields.');
+      return;
     }
+
+    const formDataToUpdateSupabase = {
+      
+      Personal_id: personalId,
+      fullName:fullName,
+      dateOfBirth: new Date(dateOfBirth).toISOString(),
+      Email: email,
+      Contact: contact,
+      password: password,
+    };
+
+    insertDataIntoSupabase(formDataToUpdateSupabase);
     
+  };
+  
+  const insertDataIntoSupabase = async (formDataToUpdateSupabase) => {
+    try {
+      const { data, error } = await supabase.from('Manager').insert([
+        {
+          Personal_id: formDataToUpdateSupabase.Personal_id,
+          full_name: formDataToUpdateSupabase.fullName,
+          DOB: formDataToUpdateSupabase.dateOfBirth,
+          email: formDataToUpdateSupabase.Email,
+          contact: formDataToUpdateSupabase.Contact,
+          password: formDataToUpdateSupabase.password,
+        },
+      ]);
+  
+      if (error) {
+        alert('Error inserting data into Supabase: ' + error.message);
+      } else {
+        alert('Data inserted into Supabase: ' + JSON.stringify(data));
+        showAlert('You are successfully registered.');
+      }
+    } catch (error) {
+      console.log('Error connecting to Supabase: ' + error.message);
+    }
+  };
+
+  return (
+    <div className="background-image4">
+      <div className="manager">
+        <div className="manager-container">
+          <form onSubmit={gatDataIntoSupabase}>
+            <h1 className="form-header" data-component="header">
+              Manager
+            </h1>
+            <div className="form-group">
+              <label htmlFor="full_name">Full Name</label>
+              <input
+                type="text"
+                className="form-control"
+                id="full_name"
+                name="full_name"
+                placeholder="Enter your full name"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="Personal_id">NIC</label>
+              <input
+                type="text"
+                className="form-control"
+                id="Personal_id"
+                name="Personal_id"
+                placeholder="Enter your NIC number"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                name="email"
+                placeholder="Enter your email address"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="DOB">Date of Birth</label>
+              <input type="date" className="form-control" id="DOB" name="DOB" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="contact">Contact</label>
+              <input
+                type="contact"
+                className="form-control"
+                id="contact"
+                name="contact"
+                placeholder="Enter your contact numbers"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                name="password"
+                placeholder="Enter your password"
+              />
+            </div>
+            <br />
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Manager;
