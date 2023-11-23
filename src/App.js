1e96b6f9bc0e01da6b071b768fcf3f5e09c721e2
ZimraMohamed <zimramohamed64@gmail.com>
2023-11-23 15:34:54 +0530

    ss

--- src/App.js
+++ src/App.js
@@ -12,8 +12,7 @@ import BuyNow from './Components/pages/BuyNow';
 import Customer from './Components/pages/Customer';
 import ExploreProducts from './Components/pages/ExploreProducts';
 import AddLocation from './Components/pages/AddLocation';
-import Loan from './Components/pages/Loan';
-import Dashboard from './Components/pages/Dashboard'
+
 
 
 
@@ -34,8 +33,7 @@ function App() {
         <Route path="ExploreProducts" element={<ExploreProducts/>}/>
         <Route path="buyNow" element={<BuyNow/>}/>
         <Route path="AddLocation" element={<AddLocation/>}/>
-        <Route path="loan" element={<Loan/>}/>
-        <Route path="Dashboard" element={<Dashboard/>}/>
+      
         
       </Routes>
       </BrowserRouter>
--- src/Components/molecules/NavBar/index.jsx
+++ src/Components/molecules/NavBar/index.jsx
@@ -34,14 +34,9 @@ const NavBar=()=>{
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="exploreproducts">ExploreProducts</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="loan">Loan</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="Dashboard">Dashboard</a>
-        </li>
+
         
-       
+        
       </ul>
     </div>
   </div>
